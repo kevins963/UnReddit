3368fab0df3c7b6e15b7611786218e2b701757ef
kevins963 <kevins963@hotmail.com>
2019-11-27 19:44:16 -0800

    Adding default project files
    Simple service for reddit

---
+++ UnReddit/IRedditApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Refit;
+
+namespace UnReddit
+{
+    class UserId
+    {
+        [JsonProperty("id")]
+        public String ID;
+    }
+
+    [Headers(new string[]
+        {
+        "User-Agent: MyRedditApp / 1.0.0",
+        "Authorization: bearer"
+        })]
+    interface IRedditApi
+    {
+        [Get("/api/v1/me")]
+        Task<UserId> GetMe();
+
+    }
+}
---
+++ UnReddit/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Refit;
+using System.Text;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnReddit
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private RedditService mRedditService = new RedditService();
+        private IRedditApi mRedditApi = null;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            mRedditApi = RestService.For<IRedditApi>("https://oauth.reddit.com", new RefitSettings()
+            {
+                AuthorizationHeaderValueGetter = async () =>
+                {
+                    var token = await mRedditService.GetToken();
+                    var result = Convert.ToBase64String(Encoding.ASCII.GetBytes(token));
+                    return await Task.FromResult(token);
+                },
+                
+            }); ;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var user = await mRedditApi.GetMe();
+            Console.WriteLine(user.ID.ToString());
+        }
+    }
+}
